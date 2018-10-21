7bb9fbd34bac6d9823bc28389e383c72209c1889
vagfsantos <vagfsantos@debian.vagfsantos>
2018-10-21 19:12:34 -0300

    Fix BookEntity

--- src/components/book/entity/BookEntity.js
+++ src/components/book/entity/BookEntity.js
@@ -2,7 +2,8 @@ import PropTypes from "prop-types";
 
 export default PropTypes.shape({
   title: PropTypes.string.isRequired,
-  thumbnail: PropTypes.string.isRequired,
+  imageLinks: PropTypes.shape({ thumbnail: PropTypes.string.isRequired })
+    .isRequired,
   subtitle: PropTypes.string,
   categories: PropTypes.arrayOf(PropTypes.string)
 });
