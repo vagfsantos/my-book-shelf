5110c51040d673bc1050a7cfdcc96529a71aa70a
vagfsantos <vagfsantos@debian.vagfsantos>
2018-10-28 13:13:06 -0300

    Search page

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -10,10 +10,25 @@ const SearchPage = () => (
           <h1 className="title is-1">Search</h1>
           <h2 className="subtitle">Type your book</h2>
 
-          <form>
-            <input />
-            <button>Buscar</button>
-          </form>
+          <div className="columns">
+            <div className="column is-half is-offset-one-quarter">
+              <form>
+                <div class="field">
+                  <div class="control has-icons-left has-icons-right">
+                    <input
+                      class="input is-success"
+                      type="text"
+                      placeholder="Text input"
+                      value="bulma"
+                    />
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-search" />
+                    </span>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </section>
