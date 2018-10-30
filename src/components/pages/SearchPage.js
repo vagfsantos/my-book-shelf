ced8df644bd96be20577ba468fadf032ad3319a0
Vagner F Santos <santos.vagnerferreira@gmail.com>
2018-10-29 21:43:01 -0300

    Refactor search page

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 
 import Shelf from "./../shelf/Shelf";
 import HeroSection from "./../HeroSection/HeroSection";
@@ -7,7 +7,7 @@ import SearchBookForm from "./../SearchBookForm/SearchBookForm";
 import { appEvent } from "../../services/events/app-event-handler";
 import * as booksAPIService from "../../services/api/books-api";
 
-class SearchPage extends PureComponent {
+class SearchPage extends Component {
   state = {
     searchedBooks: [],
     isLoading: false
@@ -18,15 +18,20 @@ class SearchPage extends PureComponent {
       this.whenSearchDidHappened(books);
     }))();
 
+  lastAPICall = null;
+
   searchBookHandler = bookQuery => {
     this.setState({ isLoading: true });
 
     if (!bookQuery.trim()) {
-      this.emptyBookState();
-      return;
+      return this.lastAPICall.then(this.emptyBookState);
     }
 
-    booksAPIService
+    return this.fetchBookResults(bookQuery);
+  };
+
+  fetchBookResults = bookQuery => {
+    this.lastAPICall = booksAPIService
       .search(bookQuery)
       .then(response => {
         if (response.error) {
@@ -55,25 +60,36 @@ class SearchPage extends PureComponent {
     });
   };
 
-  whenSearchDidHappened = books => {
-    const stored = this.state.searchedBooks.filter(book =>
-      books.find(b => b.id === book.id)
+  whenSearchDidHappened = currentStoredBooks => {
+    const searchedBooksInAnyShelf = this.state.searchedBooks.filter(
+      searchedBook =>
+        currentStoredBooks.find(storedBook => storedBook.id === searchedBook.id)
     );
-    const replace = stored.map(
-      b =>
-        books.find(found => found.id === b.id)
-          ? books.find(found => found.id === b.id)
-          : b
+    const storedBooksToReplace = searchedBooksInAnyShelf.map(
+      bookInfoUpdated => {
+        const newBookInfo = currentStoredBooks.find(
+          bookInfo => bookInfo.id === bookInfoUpdated.id
+        );
+        if (newBookInfo) {
+          return newBookInfo;
+        }
+
+        return bookInfoUpdated;
+      }
     );
 
     this.setState(previous => {
       return {
-        searchedBooks: previous.searchedBooks.map(
-          b =>
-            replace.find(found => b.id === found.id)
-              ? replace.find(found => b.id === found.id)
-              : b
-        )
+        searchedBooks: previous.searchedBooks.map(bookResult => {
+          const newBookResult = storedBooksToReplace.find(
+            newBook => newBook.id === bookResult.id
+          );
+
+          if (newBookResult) {
+            return newBookResult;
+          }
+          return bookResult;
+        })
       };
     });
   };
